2e0409a26f6d2b885f395f722eefb3fc5755e7bb
= <=>
2024-06-15 17:38:07 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Get date
+var month = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+function clock() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  var day = h < 11 ? "AM" : "PM";
+  var date = today.getDate();
+  var mon = today.getMonth();
+  var year = today.getFullYear();
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  document.getElementById("hours").innerHTML = h;
+  document.getElementById("min").innerHTML = m;
+  document.getElementById("sec").innerHTML = s;
+  document.getElementById("time").innerHTML = day;
+  document.getElementById("day").innerHTML = date;
+  document.getElementById("month").innerHTML = month[mon];
+  document.getElementById("year").innerHTML = year;
+}
+var inter = setInterval(clock, 400);
